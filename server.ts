c2308fc6a577f444353e1c77fba838ea050eb6ff
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-07-24 23:12:55 +0300

    изменение файла сервера

--- server.ts
+++ server.ts
@@ -1,21 +1,19 @@
+// @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { fileURLToPath } = require('url');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const express = require('express');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+const express = require("express");
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
 const app = express();
 
+const PORT = 3000;
 
-app.use(express.static(path.resolve(_dirname, "/dist")));
+app.use(express.static("./dist"));
 
-app.get("/", (_req: any, res: { sendFile: (arg0: any) => void; }) => {
-    res.sendFile(path.join(_dirname, "/dist/index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/dist/index.html");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server start on ${PORT} port`);
+});
 
 console.log("server started");
